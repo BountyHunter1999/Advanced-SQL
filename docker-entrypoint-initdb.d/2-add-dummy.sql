265c407637e07cb5998fe55da9459ac3b2ebcc01
shrish <adhikari.shrish1@gmail.com>
2024-07-16 21:56:07 +0545

    Updated the dummy data for more grouping

--- docker-entrypoint-initdb.d/2-add-dummy.sql
+++ docker-entrypoint-initdb.d/2-add-dummy.sql
@@ -1,8 +1,8 @@
 INSERT INTO T VALUES 
     (1, 'x', true, 10),
     (2, 'y', true, 20),
-    (3, 'x', true, 30),
-    (4, 'y', true, 40),
+    (3, 'x', false, 30),
+    (4, 'y', false, 40),
     (5, 'x', true, NULL);
 
 -- equivalent to `SELECT t.* FROM T AS t;`
